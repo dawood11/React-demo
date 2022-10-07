a45a0bf793869b578e7d7d487aa3873b626909ff
Dawood Ahmad <dawood11@hotmail.com>
2022-10-07 09:49:04 +0200

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('just a test', () => {
+  expect(1+1).toEqual(2);
+  // expect(1+1).toEqual(3);
+});
+
+test('renders calculate avg button text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByText('Regn ut gjennomsnitssalder');
+  expect(buttonElement).toBeInTheDocument();
 });
